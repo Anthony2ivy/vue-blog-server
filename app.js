4d67a807d0b7fdb0c606e86a6fd6f7ce6fe96e76
Anthony <729915632@qq.com>
2019-09-30 13:50:21 +0800

    修复content type

--- app.js
+++ app.js
@@ -13,11 +13,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.all('*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
     //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
     res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Methods', '*');
-    res.header('Content-Type', 'application/json;charset=utf-8');
+    res.header('Access-Control-Allow-Credentials','true');
+    res.header('Access-Control-Allow-Headers','content-type');
     next();
 });
 app.get('/', (req, res) => res.send('Hello World!'));
